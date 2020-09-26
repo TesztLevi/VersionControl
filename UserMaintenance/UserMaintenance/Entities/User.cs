03fcceb2ac0adde5f5dc5dcfadb5ef9608e46884
Levente Németh <NemethLevente@hotmail.hu>
2020-09-26 16:26:40 +0200

    initial comit

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserMaintenance.Entities
+{
+        public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Firtsname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname { get
+            {
+                return string.Format("{0} {1}", Lastname, Firtsname);
+            } }
+    }
+}
