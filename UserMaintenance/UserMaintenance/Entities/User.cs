2fa40b4446484ac04c6a408a58d3948303f3aa26
Levente Németh <NemethLevente@hotmail.hu>
2020-10-02 15:27:06 +0200

    Első comit

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,11 +9,7 @@ namespace UserMaintenance.Entities
         public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string Firtsname { get; set; }
-        public string Lastname { get; set; }
-        public string Fullname { get
-            {
-                return string.Format("{0} {1}", Lastname, Firtsname);
-            } }
+        public string Fullname { get; set; }
+        
     }
 }
