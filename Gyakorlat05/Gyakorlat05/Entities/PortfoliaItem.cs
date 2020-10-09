963d051d15d0cc23383ccc4fe9a4e898766a83da
Levente Németh <NemethLevente@hotmail.hu>
2020-10-09 15:17:09 +0200

    Ötodik Comit

---
+++ Gyakorlat05/Gyakorlat05/Entities/PortfoliaItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlat05.Entities
+{
+    class PortfoliaItem
+    {
+        public string Index { get; set; }
+        public decimal Volumen { get; set; }
+    }
+}
