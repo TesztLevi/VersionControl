92cf7de25c9bdcf31b3de5a182416d0a2cd721f6
Levente Németh <NemethLevente@hotmail.hu>
2020-10-17 17:38:28 +0200

    Commit 3

--- Gyakorlat06/Gyakorlat06/Form1.cs
+++ Gyakorlat06/Gyakorlat06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Gyakorlat06.MnbServiceReference;
 
 namespace Gyakorlat06
 {
@@ -15,6 +16,28 @@ namespace Gyakorlat06
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
+            //MessageBox.Show(result);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
