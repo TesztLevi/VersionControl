60a7d75a41ada2fd6eaf6abbecd3d589b1b4e590
Levente Németh <NemethLevente@hotmail.hu>
2020-10-25 21:29:07 +0100

    Commit 4.0

---
+++ Gyakorlat07/Gyakorlat07/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlat07
+{
+    public class BirthProbability
+    {
+        public int age { get; set; }
+        public int childnum { get; set; }
+        public double chance { get; set; }
+    }
+}
---
+++ Gyakorlat07/Gyakorlat07/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gyakorlat07.Entities;
+
+namespace Gyakorlat07
+{
+    public class DeathProbability
+    {
+        public int age { get; set; }
+        public Gender Gender { get; set; }
+        public double chance { get; set; }
+    }
+}
