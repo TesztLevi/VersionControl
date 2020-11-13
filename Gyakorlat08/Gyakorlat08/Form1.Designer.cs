0e6e8593392c215ec7300d79f68b9a40b4ea5068
Levente Németh <NemethLevente@hotmail.hu>
2020-11-13 21:54:34 +0100

    Commit 11.0

--- Gyakorlat08/Gyakorlat08/Form1.Designer.cs
+++ Gyakorlat08/Gyakorlat08/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblnext = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -39,9 +42,9 @@
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Location = new System.Drawing.Point(0, 74);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(800, 450);
+            this.mainPanel.Size = new System.Drawing.Size(800, 376);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -56,15 +59,48 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.ConveyorTimer_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Car";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblnext
+            // 
+            this.lblnext.AutoSize = true;
+            this.lblnext.Location = new System.Drawing.Point(12, 9);
+            this.lblnext.Name = "lblnext";
+            this.lblnext.Size = new System.Drawing.Size(103, 20);
+            this.lblnext.TabIndex = 2;
+            this.lblnext.Text = "Coming Next:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(295, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ball";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblnext);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +109,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblnext;
+        private System.Windows.Forms.Button button2;
     }
 }
 
